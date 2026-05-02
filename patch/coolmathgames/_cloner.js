0196af24903d370e392a75d8a4623c04ce9b9c37
woodle87 <woodlevibes87@gmail.com>
2026-05-02 16:22:24 -0500

    Add files via upload

---
+++ patch/coolmathgames/_cloner.js
@@ -0,0 +1,1 @@
+// coolmathgames/_cloner.js
---
+++ patch/coolmathgames/sites/default/files/cmg-ads.js
@@ -0,0 +1,8 @@
+// cmg-ads.js
+window.ClonerLog= window.ClonerLog || console.log;
+
+
+cmgAdBreak= async function() {
+    ClonerLog("cmd-ads.cmgAdBreak");
+    return ClonerAd();
+}
